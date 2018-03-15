03f45f0e023a2cac6ace995a1ed56462524467a8
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-15 11:57:10 +0100

    Fix minor issue

--- src/js/utils/roman-calculator/roman-calculator.js
+++ src/js/utils/roman-calculator/roman-calculator.js
@@ -40,20 +40,3 @@ export const toArabic = a => {
 };
 
 export const sumRomans = (a, b) => toRoman(toArabic(a) + toArabic(b));
-
-// export const toRoman = a => {
-// 	if (a >= 1000) return 'M' + toRoman(a - 1000);
-// 	if (a >= 900) return 'CM' + toRoman(a - 900);
-// 	if (a >= 500) return 'D' + toRoman(a - 500);
-// 	if (a >= 400) return 'CD' + toRoman(a - 400);
-// 	if (a >= 100) return 'C' + toRoman(a - 100);
-// 	if (a >= 90) return 'XC' + toRoman(a - 90);
-// 	if (a >= 50) return 'L' + toRoman(a - 50);
-// 	if (a >= 40) return 'XL' + toRoman(a - 40);
-// 	if (a >= 10) return 'X' + toRoman(a - 10);
-// 	if (a >= 9) return 'IX' + toRoman(a - 9);
-// 	if (a >= 5) return 'V' + toRoman(a - 5);
-// 	if (a >= 4) return 'IV' + toRoman(a - 4);
-// 	if (a >= 1) return 'I' + toRoman(a - 1);
-// 	if (a === 0) return '';
-// };
